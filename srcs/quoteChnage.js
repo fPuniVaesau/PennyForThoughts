e2d50e713bc7834bfbbb90051cbddb9635897c04
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2023-11-24 12:19:01 -0800

    Created an array of random quotes to use for the weekly quote change

---
+++ srcs/quoteChnage.js
@@ -0,0 +1,23 @@
+let randomNumberGenerator = () => {
+    let randomNum = Math.floor(Math.random()*7);
+    return randomNum;
+};
+let QuotesOFtheWeek = [
+    "Rivers know this, there is no hurry. We shall get there some day.",
+
+    "I'm a greater believer in luck, and I find the harder I work the more I have of it",
+
+    "...talent means nothing, while experience, acquired in humility and with hard work, means everything.",
+
+    "Excellence is never an accident. It is always the result of high intention, sincere effort, and intelligent execution; it represents the wise choice of many alternatives - choice, not chance, determines your destiny.",
+
+    "Learning is not child's play; we cannot learn without pain.",
+
+    "Wise men speak when they have something to say, fools speak because they have to say something",
+
+    "Man is a goal-seeking animal. His life only has meaning if he is reaching out and striving for his goals."
+]
+console.log(randomNumberGenerator());
+
+console.log(QuotesOFtheWeek[0]);
+
